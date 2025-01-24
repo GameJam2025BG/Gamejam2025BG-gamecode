01b01e80c39be8ba032c448df0304aef56c23353
zxxtlz <Hrisy2015@gmail.com>
2025-01-25 00:45:10 +0200

    Added scene change and transition

---
+++ gamejam2025bg/area_2d2.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var entered = false
+
+func _on_body_entered(body: PhysicsBody2D):
+	entered = true
+
+func _on_body_exited(body: PhysicsBody2D):
+	entered = false
+
+func _process(delta):
+	if entered and Input.is_action_just_pressed("ui_accept"):
+		TransitionScreen.transition()
+		await TransitionScreen.on_transition_finished
+		get_tree().change_scene_to_file("res://main.tscn")
