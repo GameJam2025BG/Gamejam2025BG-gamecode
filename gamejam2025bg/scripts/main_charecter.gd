c2d9acfebc7bd49602dc4316069e3ea350a3a4c6
simeonnv2 <simmeon.nv@abv.bg>
2025-01-25 10:33:56 +0200

    AIMATIONS

--- gamejam2025bg/scripts/main_charecter.gd
+++ gamejam2025bg/scripts/main_charecter.gd
@@ -1,6 +1,11 @@
 extends CharacterBody2D
+
+@onready var animation = $AnimationPlayer
+
 @export var SPEED: float = 300.0
 @export var RUN_SPEED: float = 600.0
+
+var last_direction: Vector2 = Vector2(0, -1)
 
 func _physics_process(delta: float) -> void:
 	
@@ -11,18 +16,34 @@ func _physics_process(delta: float) -> void:
 	else:
 		speed = SPEED
 	
-	# Handle movement.
-	if InputTracker.left:
-		print("left")
-		position.x -= speed * delta
-	if InputTracker.right:
-		print("right")
-		position.x += speed * delta
-	if InputTracker.up:
-		print("up")
-		position.y -= speed * delta
-	if InputTracker.down:
-		print("down")
-		position.y += speed * delta
+	
+	if InputTracker.left || InputTracker.right || InputTracker.up || InputTracker.down:
+		# Handle movement.
+		if InputTracker.left:
+			animation.play("walk_left")
+			position.x -= speed * delta
+			last_direction = Vector2(-1, 0)
+		if InputTracker.right:
+			animation.play("walk_right")
+			position.x += speed * delta
+			last_direction = Vector2(1, 0)
+		if InputTracker.up:
+			animation.play("walk_back")
+			position.y -= speed * delta
+			last_direction = Vector2(0, 1)
+		if InputTracker.down:
+			animation.play("walk_front")
+			position.y += speed * delta
+			last_direction = Vector2(0, -1)
+	else:
+		if last_direction == Vector2(0, -1):
+			animation.play("idle_front")
+		if last_direction == Vector2(0, 1):
+			animation.play("idle_back")
+		if last_direction == Vector2(1, 0):
+			animation.play("idle_left")
+		if last_direction == Vector2(-1, 0):
+			animation.play("idle_right")
+	
 
 	move_and_slide()
