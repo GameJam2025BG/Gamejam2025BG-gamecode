2478570f2250eff53024d470f6edd229010deca6
Kirill Ibragimov <hexchap@gmail.com>
2025-01-25 18:01:28 +0200

    TextBubble

--- gamejam2025bg/world.gd
+++ gamejam2025bg/world.gd
@@ -4,7 +4,15 @@ func _input(event: InputEvent) -> void:
 	if Dialogic.current_timeline != null:
 		return
 
-	if event is InputEventKey and event.keycode == KEY_H and event.pressed:
-		print("starting")
-		Dialogic.start('main')
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_H:
+			print(1)
+			var layout = Dialogic.Styles.load_style("bubble")
+			layout.register_character(load("res://dialogic/characters/Some Creature.dch"), $"NPC1Marker")
+			Dialogic.start("Some Creature I")
+		elif event.keycode == KEY_ENTER or event.keycode == KEY_KP_ENTER:
+			print(2)
+			Dialogic.Styles.load_style("boxed novela")
+			Dialogic.start('main')
+			
 		get_viewport().set_input_as_handled()
