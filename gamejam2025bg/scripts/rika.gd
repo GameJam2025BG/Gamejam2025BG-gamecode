c7536dc56f3ebafee223af52e04fa42f1c490d80
simeonnv2 <simmeon.nv@abv.bg>
2025-01-25 15:01:23 +0200

    g

--- gamejam2025bg/scripts/rika.gd
+++ gamejam2025bg/scripts/rika.gd
@@ -1,14 +1,19 @@
 extends CharacterBody2D
 
+@export var is_iteractable: bool = true
+@export var text: Array[String] = ["i have 1 dialog and no input!"]
+
 var entered = false
 
 func _ready() -> void:
 	entered = false
 
+var interacted = false
 func _process(delta: float) -> void:
 	print(entered)
-	if entered:
-		DialogManager.start_dialog(position, ["finger, hello there finger", "RczxczxcEAL"])
+	if entered && !interacted && is_iteractable:
+		DialogManager.start_dialog(position, text)
+		interacted = true
 
 func _on_body_entered(body: PhysicsBody2D):
 	entered = true
