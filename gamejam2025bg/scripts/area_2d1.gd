2d48396bbc8bf800d8fb8bc2c86229644f2f92b4
zxxtlz <Hrisy2015@gmail.com>
2025-01-25 22:00:35 +0200

    Fixed teleport script, finished main screen/menu

--- gamejam2025bg/scripts/area_2d1.gd
+++ gamejam2025bg/scripts/area_2d1.gd
@@ -1,15 +1,18 @@
 extends Area2D
 
+@export_file var dest_scene
+
 var entered = false
 
-func _on_body_entered(body: PhysicsBody2D):
+func _on_body_entered(_body: PhysicsBody2D):
 	entered = true
 
-func _on_body_exited(body: PhysicsBody2D):
+func _on_body_exited(_body: PhysicsBody2D):
 	entered = false
-
-func _process(delta):
+ 
+func _physics_process (_delta):
 	if entered and Input.is_action_just_pressed("ui_accept"):
+		print("You pressed")
 		TransitionScreen.transition()
 		await TransitionScreen.on_transition_finished
 		get_tree().change_scene_to_file("res://components/world2.tscn")
