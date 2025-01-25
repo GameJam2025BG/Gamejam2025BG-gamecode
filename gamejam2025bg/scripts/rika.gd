8b1bc9386cf9eb48a72496b622c5f2dfa157a3ea
simeonnv2 <simmeon.nv@abv.bg>
2025-01-25 18:41:04 +0200

    FINGERRRRR?!??!?!?

--- gamejam2025bg/scripts/rika.gd
+++ gamejam2025bg/scripts/rika.gd
@@ -6,6 +6,10 @@ extends CharacterBody2D
 
 @export var cutscene_name: String = "main";
 
+@export var is_bubble: bool = false
+
+@export var character: Node
+
 @export var text: Array[String] = ["i have 1 dialog and no input!"]
 
 var entered = false
@@ -15,15 +19,21 @@ func _ready() -> void:
 
 var interacted = false
 func _process(delta: float) -> void:
-	print("cutscene: ", is_cutscene)
-	print(Dialogic.current_timeline == null)
+	
+	if is_bubble:
+		var layout = Dialogic.Styles.load_style("bubble")
+		layout.register_character(load("res://dialogic/characters/Some Creature.dch"), character)
+	
 	if entered && !interacted && is_iteractable && !is_cutscene:
 		DialogManager.start_dialog(position, text)
 		interacted = true
+		print("interacted")
 	if entered && !interacted && is_iteractable && is_cutscene:
 		Dialogic.start(cutscene_name)
-		get_viewport().set_input_as_handled()
 		interacted = true
+		print("interacted")
+	
+	get_viewport().set_input_as_handled()
 
 func _on_body_entered(body: PhysicsBody2D):
 	entered = true
